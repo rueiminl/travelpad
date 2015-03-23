d5149ebe9a3fd2c81470d9b8e5f20e2879a384bc
dyshen <mr.dyshen@gmail.com>
2015-03-22 20:38:16 -0400

    change urls

--- travelpad/urls.py
+++ travelpad/urls.py
@@ -3,6 +3,4 @@ urlpatterns = patterns('',
     url('^$', 'travelpad.views.todolist', name='default'),
     url('^todolist$', 'travelpad.views.todolist', name='todolist'),
     url('^create_todo$', 'travelpad.views.create_todo', name='create_todo'),
-    
-    url('^calendar$', 'travelpad.views_schedule.calendar', name='calendar'),
 )
--- webapps/urls.py
+++ webapps/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     url(r'', include('travelpad.urls')),
+    url(r'', include('travelpad.urls_itinerary')),
 )
