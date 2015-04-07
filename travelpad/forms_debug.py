c85acef7068a157f3553390d5a16ce31f327e6dc
Ray0617 <lrm0617@gmail.com>
2015-04-06 21:36:57 -0400

    add function to update travelpaduser's photo (in debug_database)

--- travelpad/forms_debug.py
+++ travelpad/forms_debug.py
@@ -19,3 +19,16 @@ class DebugItineraryForm(forms.ModelForm):
 			raise forms.ValidationError('File too big (max size is {0} bytes)'.format(MAX_UPLOAD_SIZE))
 		return photo
 		
+class DebugTravelPadUserForm(forms.ModelForm):
+	class Meta:
+		model = TravelPadUser
+	def clean_photo(self):
+		photo = self.cleaned_data['photo']
+		if not photo:
+			return None
+		if not photo.content_type or not photo.content_type.startswith('image'):
+			raise forms.ValidationError('File type is not image')
+		if photo.size > MAX_UPLOAD_SIZE:
+			raise forms.ValidationError('File too big (max size is {0} bytes)'.format(MAX_UPLOAD_SIZE))
+		return photo
+		
