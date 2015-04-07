446910f0614bf90f70575d6a45f5d0d7ae3dfc0e
Ray0617 <lrm0617@gmail.com>
2015-04-06 20:50:41 -0400

    modify layout in itineraries

---
+++ travelpad/forms_itineraries.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.db.models.fields.files import FieldFile
+from django.core.validators import validate_email, RegexValidator
+from models import *
+
+MAX_UPLOAD_SIZE = 2500000
+       
+class ItineraryForm(forms.ModelForm):
+	class Meta:
+		model = Itinerary
+	def clean_photo(self):
+		photo = self.cleaned_data['photo']
+		if not photo:
+			return None
+		if not photo.content_type or not photo.content_type.startswith('image'):
+			raise forms.ValidationError('File type is not image')
+		if photo.size > MAX_UPLOAD_SIZE:
+			raise forms.ValidationError('File too big (max size is {0} bytes)'.format(MAX_UPLOAD_SIZE))
+		return photo
+		
