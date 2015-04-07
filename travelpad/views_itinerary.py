55424cc3ef584a881c716f4714b3ddc9e1944063
dyshen <mr.dyshen@gmail.com>
2015-04-06 23:54:04 -0400

    add reply

--- travelpad/views_itinerary.py
+++ travelpad/views_itinerary.py
@@ -93,8 +93,11 @@ def feed(request, itinerary_id):
         context['messages'] = messages
         last_update = ''
         for message in messages:
-            message.replies = Reply.objects.filter(related_message=message).order_by('creation_time')
-            # last_update = max(last_update, message.timestamp)
+            message.replies = Reply.objects.filter(related_message=message).order_by('creation_time')     
+            if message.replies:
+                message.last_update_reply = max(reply.creation_time for reply in message.replies).isoformat()
+            else :
+                message.last_update_reply = ''
         # if messages:
  #            # last_update = messages[0].creation_time.isoformat()
  #            #update_times = max(messages, key=lambda x:x['timestamp'])#[x['timestamp'] for x in messages]
@@ -104,7 +107,7 @@ def feed(request, itinerary_id):
  #        else:
  #            last_update = ''
         context['last_update'] = ''
-        context['last_update'] = max(message.timestamp for message in messages)
+        context['last_update'] = max(message.creation_time for message in messages)
     except ObjectDoesNotExist:
         return HttpResponseNotFound('<h1>Itinerary not found</h1>')
     return render(request, 'travelpad/feed.html', context)
@@ -136,10 +139,13 @@ def get_message_json(request, itinerary_id):
         itinerary = Itinerary.objects.get(id=itinerary_id)
         last_update = dateutil.parser.parse(request.POST.get('last_update'))
         # print last_update
-        messages = Message.objects.filter(timestamp__gt=last_update, related_itinerary=itinerary).order_by('-creation_time')
+        messages = Message.objects.filter(creation_time__gt=last_update, related_itinerary=itinerary).order_by('-creation_time')
         print 'get ' + str(len(messages)) + ' messages'
-        context['last_update'] = ''
-        context['last_update'] = max(message.timestamp for message in messages).isoformat()
+        if messages:
+            context['last_update'] = max(message.creation_time for message in messages).isoformat()
+        else:
+            context['last_update'] = ''
+        
         messages_json = [] 
         for message in messages:
             message.replies = Reply.objects.filter(related_message=message).order_by('creation_time')
@@ -152,17 +158,20 @@ def get_message_json(request, itinerary_id):
                     'creation_time': reply.creation_time.isoformat(),
                     # 'photo_url' : reverse('photo', kwargs={'uid':comment.created_by.id})
                 })
+            if message.replies:
+                last_update_reply = max(reply.creation_time for reply in message.replies).isoformat()
+            else:
+                last_update_reply = ''
+            
             messages_json.append(
                 {'id' : message.id,
                 'created_by_username':message.created_by.username, 
                 'created_by_uid': message.created_by.id, 
                 'content': message.content,
                 'creation_time': message.creation_time.isoformat(),
-                # 'last_update_comment' : post.last_update_comment,
+                'last_update_reply' : last_update_reply,
                 'replies' : replies_json})
         context['messages'] = messages_json
-        
-        
         # for message in messages:
 #             post.comments = Comment.objects.filter(related_post=post).order_by('creation_time')
 #             max_creation_time = Comment.objects.filter(related_post=post).order_by('creation_time').aggregate(Max('creation_time'))
@@ -205,51 +214,45 @@ def get_message_json(request, itinerary_id):
 @transaction.atomic
 def add_reply(request):
     errors = []   
-    # try:
-#         post_id = request.POST.get('post_id')
-#         last_update = request.POST.get('last_update_comment')
-#         # add comment
-#         post = Post.objects.get(id=post_id)
-#         entry = Comment(created_by=request.user, related_post=post)
-#         comment_form = CommentForm(request.POST, instance=entry)
-#         if not comment_form.is_valid():
-#             errors.append('You must enter content to comment.')
-#             return HttpResponse(json.dumps({'errors':errors}), content_type='application/json')
-#         else:
-#             comment_form.save()
-#         # query new comments
-#         comments = Comment.objects.all().filter(
-#             related_post=post,
-#             creation_time__gt=timestamp_to_datetime(last_update)
-#         ).order_by('creation_time')
-#         print 'get ' + str(len(comments)) + ' comments'
-#         comments_json = []
-#         for comment in comments:
-#             comments_json.append({
-#                 'created_by_username':comment.created_by.username,
-#                 'created_by_uid': comment.created_by.id,
-#                 'content': comment.content,
-#                 'creation_time': datetime_to_timestamp(comment.creation_time),
-#                 'photo_url' : reverse('photo', kwargs={'uid':comment.created_by.id})
-#             })
-#         # update last update time
-#         new_update = last_update
-#         max_creation_time = Comment.objects.filter(
-#             related_post=post,
-#             creation_time__gt=timestamp_to_datetime(last_update)
-#         ).order_by('creation_time').aggregate(Max('creation_time'))
-#         if max_creation_time['creation_time__max']:
-#             new_update = datetime_to_timestamp(max_creation_time['creation_time__max'])
-#         # generate json
-#         data = {
-#             'last_update_comment' : new_update,
-#             'comments': comments_json,
-#         }
-#         response_text = json.dumps(data)
-#         return HttpResponse(response_text, content_type='application/json')
-#     except ObjectDoesNotExist:
-#         return HttpResponseNotFound('<h1>Post not found</h1>')
-#     except Exception as inst:
-#         print inst
+    context = {}
+    try:
+        message_id = request.POST.get('message_id')
+        last_update_reply = dateutil.parser.parse(request.POST.get('last_update_reply'))
+        message = Message.objects.get(id=message_id)
+        entry = Reply(created_by=request.user, related_message=message)
+        reply_form = ReplyForm(request.POST, instance=entry)
+        if not reply_form.is_valid():
+            errors.append('You must enter content to reply.')
+            return HttpResponse(json.dumps({'errors':errors}), content_type='application/json')
+        else:
+            reply_form.save()
+        # query new comments
+        replies = Reply.objects.filter(
+            related_message=message,
+            creation_time__gt=last_update_reply
+        ).order_by('creation_time')
+        print 'get ' + str(len(replies)) + ' comments'
+        replies_json = []
+        for reply in replies:
+            replies_json.append({
+                'created_by_username':reply.created_by.username,
+                'created_by_uid': reply.created_by.id,
+                'content': reply.content,
+                'creation_time': reply.creation_time.isoformat(),
+                # 'photo_url' : reverse('photo', kwargs={'uid':comment.created_by.id})
+            })
+        context['replies'] = replies_json
+        # update last update time
+        if replies:
+            context['last_update_reply'] = max(reply.creation_time for reply in replies).isoformat()
+        else:
+            context['last_update_reply'] = ''
+        
+        response_text = json.dumps(context)
+        return HttpResponse(response_text, content_type='application/json')
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound('<h1>Post not found</h1>')
+    except Exception as inst:
+        print inst
     
     
