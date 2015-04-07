4de5f2a6807c99deb09152e3705f0a1532b1ab14
Ray0617 <lrm0617@gmail.com>
2015-04-06 23:30:56 -0400

    add function of adding new itinerary

--- travelpad/urls_itineraries.py
+++ travelpad/urls_itineraries.py
@@ -3,6 +3,8 @@ urlpatterns = patterns('',
     url(r'^itinerary$', 'travelpad.views_itineraries.itineraries', name='itineraries'),
     url(r'^itinerary/(?P<id>\d+)$', 'travelpad.views_itineraries.itinerary', name='itinerary'),
     url(r'^itineraries$', 'travelpad.views_itineraries.itineraries', name='itineraries'),
+    url(r'^add_itinerary_page$', 'travelpad.views_itineraries.add_itinerary_page', name='add_itinerary_page'),
     url(r'^update_itinerary_page/(?P<id>\d+)$', 'travelpad.views_itineraries.update_itinerary_page', name='update_itinerary_page'),
+    url(r'^add_itinerary$', 'travelpad.views_itineraries.add_itinerary', name='add_itinerary'),
     url(r'^update_itinerary/(?P<id>\d+)$', 'travelpad.views_itineraries.update_itinerary', name='update_itinerary'),
 )
--- travelpad/views_itineraries.py
+++ travelpad/views_itineraries.py
@@ -22,16 +22,41 @@ def itineraries(request):
 	return render(request, 'travelpad/itineraries.html', context)
 
 @login_required
+def add_itinerary_page(request):
+	context = {}
+	itinerary = Itinerary()
+	itinerary_form = ItineraryForm(instance = itinerary)
+	context = {}
+	context["itinerary_form"] = itinerary_form
+	return render(request, 'travelpad/add_itinerary_page.html', context)
+
+@login_required
 def update_itinerary_page(request, id):
 	context = {}
-	print "update_itinerary_page id = ", id
 	itinerary = get_itinerary(id)
-	print "itinerary =", itinerary
 	itinerary_form = ItineraryForm(instance = itinerary)
 	context = {}
 	context["itinerary_id"] = id
 	context["itinerary_form"] = itinerary_form
 	return render(request, 'travelpad/update_itinerary_page.html', context)
+
+@login_required
+@transaction.atomic
+def add_itinerary(request):
+	if request.method == "GET":
+		print "add_itinerary POST only"
+		return redirect("itineraries")
+	itinerary = Itinerary()
+	itinerary.created_by = request.user
+	if not request.FILES:
+		itinerary.photo = None
+	itinerary_form = ItineraryForm(request.POST, request.FILES, instance = itinerary)
+	if not itinerary_form.is_valid():
+		print "debug_add_itinerary form.is_valid fail"
+		print itinerary_form.errors
+		return redirect("itineraries")
+	itinerary_form.save()
+	return redirect("itineraries")
 
 @login_required
 @transaction.atomic
