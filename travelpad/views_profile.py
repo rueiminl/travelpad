9eca1827fab755ba40ad23699baa549e3eadf297
Ray0617 <lrm0617@gmail.com>
2015-04-12 17:25:58 -0400

    finish user profile page

--- travelpad/views_profile.py
+++ travelpad/views_profile.py
@@ -8,17 +8,23 @@ from django.contrib.auth.decorators import login_required
 from django.core import serializers
 from django.http import HttpResponse
 
-def get_user(id):
+def get_travelpaduser(id):
 	user = get_object_or_404(TravelPadUser, id=id)
 	if not user:
-		print "get_user(" + id + ") not found"
+		print "get_travelpaduser(" + id + ") not found"
 		raise Http404
 	return user
 
 @login_required
 def profile(request):
+	travelpaduser = get_travelpaduser(request.user.id)
+	travelpaduser_form = TravelPadUserForm(instance = travelpaduser)
+	travelpaduser_form.fields["first_name"].initial = request.user.first_name
+	travelpaduser_form.fields["last_name"].initial = request.user.last_name
+	travelpaduser_form.fields["email"].initial = request.user.email
+
 	context = {}
-	context["user_form"] = TravelPadUserForm()
+	context["travelpaduser_form"] = travelpaduser_form
 	return render(request, 'travelpad/profile.html', context)
 
 @login_required
@@ -33,15 +39,20 @@ def update_profile(request):
 	if request.method == "GET":
 		print "update_user POST only"
 		return redirect("profile")
-	user = TravelPadUser.objects.get(user=request.user)
-	if request.POST.get("clear"):
-		user.photo = None
-	if not request.FILES:
-		user_form = TravelPadUserFormWithoutPhoto(request.POST, instance = user)
+	request.user.first_name = request.POST['first_name']
+	request.user.last_name = request.POST['last_name']
+	request.user.email = request.POST['email']
+	request.user.save()
+	travelpaduser = get_travelpaduser(request.user.id)
+	if "photo-clear" in request.POST:
+		travelpaduser.photo = None
+		user_form = TravelPadUserForm(request.POST, instance = travelpaduser)
+	elif request.FILES:
+		user_form = TravelPadUserForm(request.POST, request.FILES, instance = travelpaduser)
 	else:
-		user_form = TravelPadUserForm(request.POST, request.FILES, instance = user)
+		user_form = TravelPadUserFormWithoutPhoto(request.POST, instance = travelpaduser)
 	if not user_form.is_valid():
-		print "update_user form.is_valid fail"
+		print "update_profile user_form.is_valid fail"
 		print user_form.errors
 		return redirect("profile")
 	user_form.save()
@@ -59,8 +70,8 @@ def delete_user(request):
 
 @login_required
 def get_user_photo(request, id):
-	user = get_user(id)
-	if not user.photo:
-		print "user[" + id + "].photo not found"
+	travelpaduser = get_travelpaduser(id)
+	if not travelpaduser.photo:
+		print "travelpaduser[" + id + "].photo not found"
 		raise Http404
-	return HttpResponse(user.photo, content_type="image")
+	return HttpResponse(travelpaduser.photo, content_type="image")
